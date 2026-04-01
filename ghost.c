b45c9df538c202b9b72d3e84983688268e2b2ec3
Ankush <mondalankush9851@gmail.com>
2026-04-01 17:59:18 +0530

    license

--- ghost.c
+++ ghost.c
@@ -1,5 +1,19 @@
+/*
+ * Copyright 2024-2026 Ankush Mondal
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #include "ghost.h"
-
 
 int mode = -1;
 const char* proto_str[LIMIT] = { "tcp", "udp", "http", "ws" };
@@ -55,8 +69,6 @@ static void ghost_parse_args(int argc, char *argv[]) {
 }
 
 void ghost_print_usage() {
-    printf("\n=== Ghost SSH Tunnel Usage ===\n\n");
-
     printf("SERVER MODE:\n");
     printf("  ghost server --port <http_port> --ssh <ssh_port>\n");
     printf("    --port : Port where WebSocket/HTTP server runs (e.g. 7777)\n");
